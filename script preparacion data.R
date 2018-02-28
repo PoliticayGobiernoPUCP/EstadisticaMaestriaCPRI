7bade7b07976f1a1b379f11d7a84a92cfbc8c34d
mgbecerra <30406984+mgbecerra@users.noreply.github.com>
2018-02-28 00:43:29 -0500

    transformar data

--- script preparacion data.R
+++ script preparacion data.R
@@ -1,41 +1,34 @@
 ##mapa provincas----
 #minusculas y tudes
 library(foreign)
-mapa<-read.dbf("provinciasPeru.dbf")###tiene acentos y ñs 
-#nota: no hay informacion de datem del marañon: lo voy a eliminar de las otras bases 
+mapa<-read.dbf("PROVINCIAsPeru.dbf")###tiene problemas en los caracteres 
 
-mapa$Provincia <- toupper(mapa$NAME_2) #crear nueva variable con mayusculas 
-library(stringi) 
-mapa$Provincia <- stri_trans_general(mapa$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+mapa$PROVINCIA <- gsub("\xd1", "N", mapa$PROVINCIA) #solucionar problema de encoding 
 
-#HUANUCO está escrito como HUENUCO:
-mapa$Provincia[mapa$Provincia == "HUENUCO"] <- "HUANUCO"
+#ojo con Callao, Carlos Fermin Fitzcarrald y Daniel Alcides Carrion que estan escritos de manera diferente en todas las bases
 
-#ojo con Callao y con Carlos Fermin Fitzcarrald y Daniel Alcides Carrion que estan escritos de manera diferente en todas las bases
-
-write.dbf(mapa, file="provinciasPeru.dbf")
+write.dbf(mapa, file="PROVINCIAsPeru.dbf")
 
 #### idh Peru ----
-#provincias en 2007 # minusculas y tildes
+#PROVINCIAs en 2007 # minusculas y tildes
 library(openxlsx)
 idhPeru<- read.xlsx("idhPeru.xlsx",skipEmptyRows = T, skipEmptyCols = T, sheet=4, startRow = 5)
 
 idhPeru <- idhPeru[-c(1435,2056:2061),]#eliminar filas innecesarias 
-idhPeru <- idhPeru[is.na(idhPeru$X2),] #seleccionar provincias 
+idhPeru <- idhPeru[is.na(idhPeru$X2),] #seleccionar PROVINCIAs 
 idhPeru <- idhPeru[,-c(2,5,7,9,11,13,15)]#eliminar columnas
 
-colnames(idhPeru) <- c("Ubigeo", "Provincia", "habitantes", "IDH", "esperanza", "secundaria", "educa", "percapitaf")
+colnames(idhPeru) <- c("Ubigeo", "PROVINCIA", "habitantes", "IDH", "esperanza", "secundaria", "educa", "percapitaf")
 rownames(idhPeru) <- 1:nrow(idhPeru)
 
-idhPeru$Provincia <- toupper(idhPeru$Provincia) # mayusculas 
-idhPeru$Provincia <- stri_trans_general(idhPeru$Provincia,"Latin-ASCII") #quitar tildes y ñs 
-##Callao y Carlos Fermin estan bien. arreglar DANIEL A. CARRION
-idhPeru$Provincia[idhPeru$Provincia == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
-##Problema con Satipo:
-idhPeru$Provincia[idhPeru$Provincia == "SATIPO 2/"] <- "SATIPO"
+idhPeru$PROVINCIA <- toupper(idhPeru$PROVINCIA) # mayusculas 
+idhPeru$PROVINCIA <- stri_trans_general(idhPeru$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 
-#eliminar datem del marañon
-idhPeru <- idhPeru[!idhPeru$Provincia=="DATEM DEL MARANON",]
+##Callao y Carlos Fermin estan bien. arreglar DANIEL A. CARRION
+idhPeru$PROVINCIA[idhPeru$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
+idhPeru$PROVINCIA[idhPeru$PROVINCIA == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
+##Problema con Satipo:
+idhPeru$PROVINCIA[idhPeru$PROVINCIA == "SATIPO 2/"] <- "SATIPO"
 
 write.xlsx(idhPeru, "idhPeru.xlsx")
 
@@ -44,12 +37,10 @@ write.xlsx(idhPeru, "idhPeru.xlsx")
 idh_elec <- read.csv("idh_elec.csv",strip.white = T,stringsAsFactors = F)
 idh_elec <- as.data.frame(aggregate(cbind(PPK, FP) ~ ubiProv + prov, data = idh_elec, sum, na.rm = F))
 
-colnames(idh_elec) <- c("Ubigeo", "Provincia", "Voto_PPK", "Voto_FP")
+colnames(idh_elec) <- c("Ubigeo", "PROVINCIA", "Voto_PPK", "Voto_FP")
 ##Callao y Carlos Fermin estan bien. arreglar DANIEL A. CARRION
-idh_elec$Provincia[idh_elec$Provincia == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
-
-#eliminar datem del marañon
-idh_elec <- idh_elec[!idh_elec$Provincia=="DATEM DEL MARANON",]
+idh_elec$PROVINCIA[idh_elec$PROVINCIA == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
+idh_elec$PROVINCIA[idh_elec$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
 
 write.xlsx(idh_elec, "idh_elec.xlsx")
 
@@ -58,24 +49,22 @@ write.xlsx(idh_elec, "idh_elec.xlsx")
 #mayusculas y sin tildes  pero con ñs
 fecundidad<- read.xlsx("fecundidad.xlsx",skipEmptyRows = T, skipEmptyCols = T, startRow = 3)
 
-fecundidad$prov <- substr(fecundidad$Ubigeo,5,6) #identificar provincias y departamentos: 00
+fecundidad$prov <- substr(fecundidad$Ubigeo,5,6) #identificar PROVINCIAs y departamentos: 00
 fecundidad$dep<- substr(fecundidad$Ubigeo,3,4) #identificar departamentos: 00
-fecundidad<- fecundidad[!fecundidad$dep=="00" & fecundidad$prov=="00",][,c(1:5)] #seleccionar provincias
+fecundidad<- fecundidad[!fecundidad$dep=="00" & fecundidad$prov=="00",][,c(1:5)] #seleccionar PROVINCIAs
 
 fecundidad<- fecundidad[complete.cases(fecundidad),]
 
-colnames(fecundidad) <- c("Ubigeo", "Provincia", "mortalidadinf", "fecundidad", "desnutricion")
+colnames(fecundidad) <- c("Ubigeo", "PROVINCIA", "mortalidadinf", "fecundidad", "desnutricion")
 rownames(fecundidad) <- 1:nrow(fecundidad)
 
 fecundidad[c(3:5)] =lapply(fecundidad[c(3:5)],as.numeric) #volverlas numericas 
 
-fecundidad$Provincia <- stri_trans_general(fecundidad$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+fecundidad$PROVINCIA <- stri_trans_general(fecundidad$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 
-##Callao está bien carlos fermin  esta como CARLOS F. FITZCARRALD transformar:
-fecundidad$Provincia[fecundidad$Provincia == "CARLOS F. FITZCARRALD"] <- "CARLOS FERMIN FITZCARRALD"
+#solucionar problema con datem del maranon 
 
-#eliminar datem del marañon
-fecundidad <- fecundidad[!fecundidad$Provincia=="DATEM DEL MARANON ",]
+fecundidad[fecundidad$PROVINCIA=="DATEM DEL MARANON ",] <- "DATEM DEL MARANON"
 
 write.xlsx(fecundidad, "fecundidad.xlsx")
 
@@ -86,20 +75,18 @@ indicadores<- read.xlsx("indicadoresvarios.xlsx",skipEmptyRows = T, skipEmptyCol
 
 indicadores <- indicadores[,-c(1,4, 7: 14)]#eliminar columnas innecesarias
 
-colnames(indicadores) <- c("Departamento", "Provincia", "densidadpob", "mortalidadinf", "analfabetismo", 
+colnames(indicadores) <- c("Departamento", "PROVINCIA", "densidadpob", "mortalidadinf", "analfabetismo", 
                            "analfabetismo_urb", "analfabetismo_rural", "analfabetismo_h", "analfabetismo_m") 
        
 #analfabetismo rural está como caracter 
 indicadores$analfabetismo_rural<- as.numeric(indicadores$analfabetismo_rural)
 
-indicadores$Provincia <- toupper(indicadores$Provincia) #mayusculas 
-indicadores$Provincia <- stri_trans_general(indicadores$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+indicadores$PROVINCIA <- toupper(indicadores$PROVINCIA) #mayusculas 
+indicadores$PROVINCIA <- stri_trans_general(indicadores$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 
 #carlos fermin esta bien. arreglar callao:
-indicadores$Provincia[indicadores$Provincia == "PROVINCIA CONSTITUCIONAL DEL CALLAO"] <- "CALLAO"
-
-#eliminar datem del marañon
-indicadores <- indicadores[!indicadores$Provincia=="DATEM DEL MARANON",]
+indicadores$PROVINCIA[indicadores$PROVINCIA == "PROVINCIA CONSTITUCIONAL DEL CALLAO"] <- "CALLAO"
+indicadores$PROVINCIA[indicadores$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
 
 write.xlsx(indicadores, "indicadoresvarios.xlsx")
 
@@ -109,16 +96,14 @@ write.xlsx(indicadores, "indicadoresvarios.xlsx")
 poblacion<- read.xlsx("poblacion.xlsx",skipEmptyRows = T, skipEmptyCols = T ,startRow = 7)
 
 poblacion <- poblacion[,-c(1,4)]#eliminar columnas innecesarias
-colnames(poblacion) <- c("Departamento", "Provincia", "pob", "pob_ur", "pob_rural", "pob_h", "pob_m")
+colnames(poblacion) <- c("Departamento", "PROVINCIA", "pob", "pob_ur", "pob_rural", "pob_h", "pob_m")
 
-poblacion$Provincia <- toupper(poblacion$Provincia) #mayusculas 
-poblacion$Provincia <- stri_trans_general(poblacion$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+poblacion$PROVINCIA <- toupper(poblacion$PROVINCIA) #mayusculas 
+poblacion$PROVINCIA <- stri_trans_general(poblacion$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 
 #carlos fermin esta bien. arreglar callao:
-poblacion$Provincia[poblacion$Provincia == "PROVINCIA CONSTITUCIONAL DEL CALLAO"] <- "CALLAO"
-
-#eliminar datem del marañon
-poblacion <- poblacion[!poblacion$Provincia=="DATEM DEL MARANON",]
+poblacion$PROVINCIA[poblacion$PROVINCIA == "PROVINCIA CONSTITUCIONAL DEL CALLAO"] <- "CALLAO"
+poblacion$PROVINCIA[poblacion$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
 
 write.xlsx(poblacion, "poblacion.xlsx")
 
@@ -127,22 +112,20 @@ write.xlsx(poblacion, "poblacion.xlsx")
 #minusculas con tildes
 ingreso<- read.xlsx("Ingreso-Per-Capita.xlsx",skipEmptyRows = T, skipEmptyCols = T ,startRow = 3)
 ingreso <- ingreso[is.na(ingreso$Distrito),] #eliminar distritos
-ingreso <- ingreso[!is.na(ingreso$Provincia),] #seleccionar provincias
+ingreso <- ingreso[!is.na(ingreso$Provincia),] #seleccionar PROVINCIAs
 
 ingreso <- ingreso[,c(1:3,5)]#seleccionar columas 
-colnames(ingreso) <- c("Ubigeo", "Departamento", "Provincia", "percapitaf")
+colnames(ingreso) <- c("Ubigeo", "Departamento", "PROVINCIA", "percapitaf")
 
-ingreso$Provincia <- toupper(ingreso$Provincia) #mayusculas 
-ingreso$Provincia <- stri_trans_general(ingreso$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+ingreso$PROVINCIA <- toupper(ingreso$PROVINCIA) #mayusculas 
+ingreso$PROVINCIA <- stri_trans_general(ingreso$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 
 ##No hay info de Callao y Carlos Fermin estan bien. arreglar DANIEL A. CARRION
-ingreso$Provincia[ingreso$Provincia == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
-
-#eliminar datem del marañon
-ingreso <- ingreso[!ingreso$Provincia=="DATEM DEL MARANON",]
+ingreso$PROVINCIA[ingreso$PROVINCIA == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
+ingreso$PROVINCIA[ingreso$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
 
 ##Problema con Satipo:
-ingreso$Provincia[ingreso$Provincia == "SATIPO 2/"] <- "SATIPO"
+ingreso$PROVINCIA[ingreso$PROVINCIA == "SATIPO 2/"] <- "SATIPO"
 
 write.xlsx(ingreso, "Ingreso-Per-Capita.xlsx")
 
@@ -151,17 +134,21 @@ write.xlsx(ingreso, "Ingreso-Per-Capita.xlsx")
 idePeru<- read.xlsx("idePeru.xlsx",skipEmptyRows = T, skipEmptyCols = T, sheet=4, startRow = 6)
 
 idePeru <- idePeru[-c(221:224),]#eliminar filas innecesarias 
-idePeru <- idePeru[is.na(idePeru$X2),] #seleccionar provincias 
+idePeru <- idePeru[is.na(idePeru$X2),] #seleccionar PROVINCIAs 
 idePeru <- idePeru[,-c(2,5,7,9,11,13,15,17)]#eliminar columnas innecesarias
-colnames(idePeru) <- c("Ubigeo", "Provincia", "habitantes", "IDE", "identidad", "salud", "educacion", "saneamiento", "electrificacion")
+colnames(idePeru) <- c("Ubigeo", "PROVINCIA", "habitantes", "IDE", "identidad", "salud", "educacion", "saneamiento", "electrificacion")
 
-idePeru$Provincia <- toupper(idePeru$Provincia) #mayusculas 
-idePeru$Provincia <- stri_trans_general(idePeru$Provincia,"Latin-ASCII") #quitar tildes y ñs 
+idePeru$PROVINCIA <- toupper(idePeru$PROVINCIA) #mayusculas 
+idePeru$PROVINCIA <- stri_trans_general(idePeru$PROVINCIA,"Latin-ASCII") #quitar tildes y ñs 
 ##Callao y Carlos Fermin estan bien. arreglar DANIEL A. CARRION
-idePeru$Provincia[idePeru$Provincia == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
-##Problema con Satipo:
-idePeru$Provincia[idePeru$Provincia == "SATIPO 1/"] <- "SATIPO"
+idePeru$PROVINCIA[idePeru$PROVINCIA == "DANIEL A. CARRION"] <- "DANIEL ALCIDES CARRION"
+idePeru$PROVINCIA[idePeru$PROVINCIA == "CARLOS FERMIN FITZCARRALD"] <- "CARLOS F. FITZCARRALD"
 
-#eliminar datem del marañon
-idePeru <- idePeru[!idePeru$Provincia=="DATEM DEL MARANON",]
+##Problema con Satipo:
+idePeru$PROVINCIA[idePeru$PROVINCIA == "SATIPO 1/"] <- "SATIPO"
+
 write.xlsx(idePeru, "idePeru.xlsx")
+
+d3<-merge(mapa,poblacion, by.x="PROVINCIA", sort=FALSE, all.x = T, all.y = T) 
+
+
